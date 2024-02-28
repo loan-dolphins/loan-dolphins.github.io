f329612e0e18e22e44874dcac22e58bd274a0382
Ahmad-A0 <71089234+Ahmad-A0@users.noreply.github.com>
2024-02-28 23:52:49 +0000

    .

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,7 +2,7 @@ let Header = () => {
     return (
         <header className="w-full bg-transparent py-10 px-5">
             <div className="container mx-auto flex justify-between items-center w-full h-full backdrop-blur-lg text-white rounded-full px-5 py-2 hover:backdrop-blur-3xl flex-col md:flex-row">
-                <h1 className="text-3xl font-bold">Dolphin Loans 🐬</h1>
+                <h1 className="text-3xl font-bold ">Dolphin Loans 🐬</h1>
                 <nav>
                     <ul className="flex space-x-4 text-neutral-200">
                         {[
